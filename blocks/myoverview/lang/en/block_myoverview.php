1d74f0f77de5309fae0527ea4cfaf50a65fc113c
Tetiana Blindaruk <t.blindaruk@gmail.com>
2018-10-15 23:26:49 +0300

    init commit

---
+++ blocks/myoverview/lang/en/block_myoverview.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings for the My overview block.
+ *
+ * @package    block_myoverview
+ * @copyright  Mark Nelson <markn@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['future'] = 'Future';
+$string['inprogress'] = 'In progress';
+$string['morecourses'] = 'More courses';
+$string['myoverview:addinstance'] = 'Add a new course overview block';
+$string['myoverview:myaddinstance'] = 'Add a new course overview block to Dashboard';
+$string['nocourses'] = 'No courses';
+$string['nocoursesinprogress'] = 'No in progress courses';
+$string['nocoursesfuture'] = 'No future courses';
+$string['nocoursespast'] = 'No past courses';
+$string['past'] = 'Past';
+$string['pluginname'] = 'Course overview';
+$string['viewcourse'] = 'View course';
+$string['viewcoursename'] = 'View course {$a}';
+$string['privacy:metadata'] = 'The myoverview block does not store any personal data.';
