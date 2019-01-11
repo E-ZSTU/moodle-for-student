c3100420dc2727352a7b54c7b1a137f546a818d1
Maksym Novik <novik.kor@gmail.com>
2019-01-11 23:41:13 +0200

    Plugin initial commit. Added custom section to admin settings.

---
+++ admin/tool/student_categories/lang/en/tool_student_categories.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'tool_student_categories', language 'en'.
+ *
+ * @package    tool_student_categories
+ * @copyright  2016 Adrian Greeve <adriangreeve.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['plugin_name'] = 'Student Course Categories';
+$string['import_checkbox'] = 'Import course categories from rozklad.ztu.edu.ua/';
+//$string['backup_checkbox'] = 'Create DB backup';
+$string['import'] = 'Import';
+$string['import_checkbox_description'] = 'Initialize import after `Save changes` button is pressed';
+$string['import_checkbox_backup_description'] = 'Backup DB tables before import';
